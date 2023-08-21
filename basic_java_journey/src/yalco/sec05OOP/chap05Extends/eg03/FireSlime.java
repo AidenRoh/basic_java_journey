9f157f6722670802274cf6d2ff80d0c026f17636
AidenRoh <colohrby@gmail.com>
2023-08-21 23:55:01 +0900

    230821

---
+++ basic_java_journey/src/yalco/sec05OOP/chap05Extends/eg03/FireSlime.java
@@ -0,0 +1,10 @@
+package yalco.sec05OOP.chap05Extends.eg03;
+
+public class FireSlime extends Slime {
+    private int fireAttack = 4;
+
+    @Override
+    public void attack (Slime enemy) {
+        enemy.hp -= (attack + fireAttack) * (1 - enemy.defense);
+    }
+}
